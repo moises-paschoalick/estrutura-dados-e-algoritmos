24de1b9b8412307a3a928a89a451a35082bf54a8
Moisés Guilherme <moisescomp@gmail.com>
2025-11-12 07:54:26 -0300

    Solução do problema dominio-email

--- COURSE/JS/STRING/Exercicios/dominio.js
+++ COURSE/JS/STRING/Exercicios/dominio.js
@@ -1,4 +1,4 @@
-function extractEmailInformation(email) {
+/*function extractEmailInformation(email) {
     const user = email.split("@")[0];
     const domain = email.split("@")[1];
     const isBrazil = domain.endsWith(".br") ? "sim": "nao";
@@ -9,4 +9,22 @@ function extractEmailInformation(email) {
 
 console.log(extractEmailInformation("joao.silva23@yahoo.com.br"));
 console.log(extractEmailInformation("maria123@gmail.com"));
+*/
 
+// Solução Nelio
+function extractEmailInformation(email) {
+
+    const parts = email.split("@");
+    const username = parts[0];
+    const domain = parts[1];
+
+    const isBrazilian = domain.endsWith(".bir");
+
+    return { username, domain, isBrazilian};
+
+}
+
+const emailInfo = extractEmailInformation("maria123@gmail.com");
+console.log("Usuario:", emailInfo.username);
+console.log("Domínio:", emailInfo.domain);
+console.log("Brasileiro:", emailInfo.isBrazilian ? "sim" : "não");

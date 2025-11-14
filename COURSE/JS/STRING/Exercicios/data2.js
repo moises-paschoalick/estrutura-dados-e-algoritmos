a896a004514b374153710dea6ac151848395d41c
Moisés Guilherme <moisescomp@gmail.com>
2025-11-14 06:51:07 -0300

    Solução do problema data2 - com padStart

--- COURSE/JS/STRING/Exercicios/data2.js
+++ COURSE/JS/STRING/Exercicios/data2.js
@@ -1,3 +1,26 @@
+// Com padStart
+function formatDate(day, month, year) {
+        
+    const formatDay = String(day).padStart(2, '0');
+    const formatMonth = String(month).padStart(2, '0');
+    
+    return `${formatDay}/${formatMonth}/${addZero(year)}`;    
+}
+/*
+function formatDate(day, month, year) {
+    return `${addZero(day)}/${addZero(month)}/${addZero(year)}`;    
+}*/
+
+function addZero(n) {
+    if (n < 10) {
+        return `0${n}`;
+    }
+    return `${n}`;
+}
+
+
+
+/*
 function extractDateData(date) {
     const dataJson = JSON.parse(date);    
     return `${dataJson.day}/${dataJson.month}/${dataJson.year}`;    
@@ -6,8 +29,11 @@ function extractDateData(date) {
 const jsonString = '{"day": "21", "month": "7", "year": "2010"}';
 
 const dataResult = extractDateData(jsonString);
+*/
 
-console.log(`${dataResult}`);
+//console.log(`${dataResult}`);
+
+console.log(formatDate(21,7,2010));
 
 
 

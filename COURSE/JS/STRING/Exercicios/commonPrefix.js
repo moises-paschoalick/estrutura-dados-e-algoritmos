59f8a20d28dcdb492061d10230fdd8fd7f716288
Moisés Guilherme <moisescomp@gmail.com>
2026-01-29 08:06:58 -0300

    Tentativa resolução prefixo-comum sem ordenação

--- COURSE/JS/STRING/Exercicios/commonPrefix.js
+++ COURSE/JS/STRING/Exercicios/commonPrefix.js
@@ -1,12 +1,23 @@
 // Problema "prefixo-comun" - longest-common-prefix (Leetcode)
 // Fazer: Write a function to find the longest common prefix string amongst an array of strings. If there is no common prefix, return an empty string "".
 
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function(strs) {
+  if(strs.length == 0) return ""; 
+  if(strs.length == 1) return strs[0]; 
 
-function longestCommonPrefix(v){
-   let prefix = checkCommonWords(v[0],v[1]);
-   prefix =  checkCommonWords(prefix,v[2]);
+  let last = strs.length-1; 
+  let prefix; 
    
-   return prefix; 
+  for(i=0; i < last; i++){
+     prefix = checkCommonWords(strs[i], strs[i+1]);
+  }
+     
+  if(prefix.length == 0) return "";
+  return prefix.join('') ?? ""; 
 }
 
 function checkCommonWords(v1, v2){
@@ -15,14 +26,17 @@ function checkCommonWords(v1, v2){
     let word1 = words[0];
     let word2 = words[1];
     let longest = [];
+
+    //console.log(word1 + " -" + word2)
+    let count = 0;
     
-    for(let i=0; i < word1.length; i++) { 
-        for(let j=0; j < word2.length; j++){
-          if(word1[i] == word2[j]) 
-            longest[i] = word1[i];
-        }
+    //TODO Testar comparar com a menor letra (não percorrer todas as letras do maior)
+    //maior comparar letra letra 
+    for(let i=0; i < word2.length; i++) { 
+        if(word1[i] === word2[i]) longest[i] = word1[i];
+        else break;
     } 
-    
+
     return longest;
 }
 
@@ -39,12 +53,19 @@ function longestVector(v1, v2){
        shortestWord = word1;
     }        
 
-    return [longestWord,shortestWord];
+    return [longestWord, shortestWord];
 }
 
-
+// case 1
 let words = ["flowers", "flow", "flight"];
+// case 2
+let words2 = ["dog", "racecar", "car"];
+// case 3
+let words3 = [""];
+// case 4
+let words4 = ["reflower","flow","flight"]
 
-console.log(longestCommonPrefix(words)); 
+console.log(longestCommonPrefix(words4)); 
 
-
+//console.log(checkCommonWords("racecar","car"));
+//console.log(checkCommonWords("car","car"));
